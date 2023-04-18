0f029af86060b8bd63f485ad0ade3ce5f92c87a0
CahenTaylor <cahentaylor@gmail.com>
2023-04-18 16:11:25 -0600

    tests passing

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
 // Code EyesOnMe Component Here
+import React from "react";
+function handleOnFocus () {
+    console.log("Good!")
+}
+function handleOnBlur () {
+    console.log("Hey! Eyes on me!")
+}
+function EyesOnMe () {
+    return(
+        <button onFocus={handleOnFocus} onBlur={handleOnBlur}>Eyes on me</button>
+    )
+}
+export default EyesOnMe
